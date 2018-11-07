fc865ce5910cf9a0bbd8f255fa6039ee7f0c80fc
Cynthia Cao <caochengzij@outlook.com>
2018-11-07 10:40:18 -0600

    nav bar fixed

--- menu.js
+++ menu.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $('.menu-trigger').click(function () {
-        $('nav ul').slideToggle(500);
+        $('nav #nav').slideToggle(500);
     });
 
     $(window).resize(function () {
         if ($(window).width() > 700){
-        $('nav ul').removeAttr('style');
+        $('nav #nav').removeAttr('index');
     }
     });
 });
